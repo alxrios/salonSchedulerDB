b7ecc79b05094012d81f16aef9ac267bc7242edf
Alejandro Ríos <76874671+alxrios@users.noreply.github.com>
2023-01-23 18:15:51 +0100

    Add files via upload

---
+++ salon.sh
@@ -0,0 +1,66 @@
+#! /bin/bash
+
+PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
+
+echo -e "\n~~~~~ MY SALON ~~~~~\n"
+
+SERVICES_LIST(){
+  echo -e "\nWelcome to My Salon, how can I help you?"
+  SERVICES=$($PSQL "SELECT * FROM services ORDER BY service_id")
+  echo "$SERVICES" | while read SERVICE_ID BAR SERVICE_NAME
+  do
+    echo "$SERVICE_ID) $SERVICE_NAME"
+  done
+}
+
+#SERVICES_MENU(){
+#  if [[ $1 ]]
+#  then
+#    echo -e "\n$1"
+#  fi
+#  SERVICES_LIST
+#  read SERVICE_ID_SELECTED
+#  case $SERVICE_ID_SELECTED in
+#    1) ;;
+#    2) ;;
+#3   3) ;;
+#    4) ;;
+#    5) ;;
+#    *) SERVICES_MENU "Please enter a valid option." ;;
+#  esac
+#}
+
+SERVICES_MENU(){
+  if [[ $1 ]]
+  then
+    echo -e "\n$1"
+  fi
+  
+  SERVICES_LIST
+
+  read SERVICE_ID_SELECTED
+  if [[ ! $SERVICE_ID_SELECTED =~ ^[1-5]+$ ]]
+  then
+    SERVICES_MENU "I could not find that service. What would you like today?"
+  else
+    echo -e "\nWhat's your phone number?"
+    read CUSTOMER_PHONE
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
+    if [[ -z $CUSTOMER_NAME ]]
+    then
+      # ASK FOR NAME
+      echo -e "\nI don't have a record for that phone number, what's your name?"
+      read CUSTOMER_NAME
+      INSERT_NEW_CUSTOMER=$($PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
+    fi
+    #FORMATED_NAME=echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g'
+    echo -e "\nWhat time would you like your cut, $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')?"
+    read SERVICE_TIME
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+    INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+    SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+    echo -e "\nI have put you down for a$SERVICE_NAME at $SERVICE_TIME, $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')."
+  fi
+}
+
+SERVICES_MENU
